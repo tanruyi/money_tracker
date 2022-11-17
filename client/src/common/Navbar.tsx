b301a3308ea9a864999937a3955e398e44bc164f
ruyi <tan_ruyi@hotmail.com>
2022-11-17 19:36:34 +0800

    added change in styling for active page on Navbar

--- client/src/common/Navbar.tsx
+++ client/src/common/Navbar.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import styles from "./Navbar.module.css";
 import { Link } from "react-router-dom";
 import { useCurrentUserContext } from "../context/currentUserContext";
+import { useLocation } from "react-router-dom";
 
 /* ====================================================
 // Type Declaration
@@ -14,6 +15,8 @@ interface NavbarProps {
 }
 
 const Navbar = ({ updateCurrentViewPage }: NavbarProps) => {
+	const location = useLocation();
+
 	/* ====================================================
     // Context
     ==================================================== */
@@ -34,10 +37,12 @@ const Navbar = ({ updateCurrentViewPage }: NavbarProps) => {
 	const adminLink = (
 		<div className={styles.pageLink}>
 			<Link to="/admin">
-				<h1 className={styles.pageTitle}>Admin</h1>
+				<h1 className={location.pathname === "/admin" ? styles.pageTitleActive : styles.pageTitle}>Admin</h1>
 			</Link>
 		</div>
 	);
+
+	console.log(location.pathname);
 
 	return (
 		// Holding container
@@ -51,37 +56,37 @@ const Navbar = ({ updateCurrentViewPage }: NavbarProps) => {
 				{/* Links to different pages */}
 				<div className={styles.pageLink}>
 					<Link to="/daily" onClick={() => handleViewClick("Daily")}>
-						<h1 className={styles.pageTitle}>Daily</h1>
+						<h1 className={location.pathname === "/daily" ? styles.pageTitleActive : styles.pageTitle}>Daily</h1>
 					</Link>
 				</div>
 				<div className={styles.pageLink}>
 					<Link to="/weekly" onClick={() => handleViewClick("Weekly")}>
-						<h1 className={styles.pageTitle}>Weekly</h1>
+						<h1 className={location.pathname === "/weekly" ? styles.pageTitleActive : styles.pageTitle}>Weekly</h1>
 					</Link>
 				</div>
 				<div className={styles.pageLink}>
 					<Link to="/monthly" onClick={() => handleViewClick("Monthly")}>
-						<h1 className={styles.pageTitle}>Monthly</h1>
+						<h1 className={location.pathname === "/monthly" ? styles.pageTitleActive : styles.pageTitle}>Monthly</h1>
 					</Link>
 				</div>
 				<div className={styles.pageLink}>
 					<Link to="/ytd" onClick={() => handleViewClick("YTD")}>
-						<h1 className={styles.pageTitle}>YTD</h1>
+						<h1 className={location.pathname === "/ytd" ? styles.pageTitleActive : styles.pageTitle}>YTD</h1>
 					</Link>
 				</div>
 				<div className={styles.pageLink}>
 					<Link to="/budget">
-						<h1 className={styles.pageTitle}>Budget</h1>
+						<h1 className={location.pathname === "/budget" ? styles.pageTitleActive : styles.pageTitle}>Budget</h1>
 					</Link>
 				</div>
 				<div className={styles.pageLink}>
 					<Link to="/analyse">
-						<h1 className={styles.pageTitle}>Analyse</h1>
+						<h1 className={location.pathname === "/analyse" ? styles.pageTitleActive : styles.pageTitle}>Analyse</h1>
 					</Link>
 				</div>
 				<div className={styles.pageLink}>
 					<Link to="/settings">
-						<h1 className={styles.pageTitle}>Settings</h1>
+						<h1 className={location.pathname === "/settings" ? styles.pageTitleActive : styles.pageTitle}>Settings</h1>
 					</Link>
 				</div>
 
