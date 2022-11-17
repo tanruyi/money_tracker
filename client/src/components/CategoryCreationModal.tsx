404e241c59ae9c2366fa71dbba04c512955a9689
ruyi <tan_ruyi@hotmail.com>
2022-11-17 10:39:27 +0800

    moved category creation modal to its own component

---
+++ client/src/components/CategoryCreationModal.tsx
@@ -0,0 +1,116 @@
+/** @format */
+
+import React, { useState } from "react";
+import { useCurrentUserContext } from "../context/currentUserContext";
+import { createCategoryAPI } from "../apis/categories";
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, FormControl, InputLabel, MenuItem, OutlinedInput, Select, TextField, Stack } from "@mui/material";
+
+/* ====================================================
+// Type Declaration
+==================================================== */
+interface newCategoryData {
+	userId: number;
+	recordId: number;
+	categoryName: string;
+}
+
+interface CategoryCreationModalProps {
+	openModal: boolean;
+	handleClickOpen: () => void;
+	handleClose: () => void;
+}
+
+const CategoryCreationModal = ({ openModal, handleClickOpen, handleClose }: CategoryCreationModalProps) => {
+	/* ====================================================
+    // Context
+    ==================================================== */
+
+	const { currentUserId, refreshData } = useCurrentUserContext();
+
+	/* ====================================================
+    // Create new category modal
+    ==================================================== */
+
+	const [error, setError] = useState<any>();
+
+	// Controls the value of select field
+	const [recordType, setRecordType] = useState<string>("");
+
+	// Controls the value for text field
+	const [categoryName, setCategoryName] = useState<string>("");
+
+	const handleRecordType = (e: any) => {
+		setRecordType(e.target.value);
+	};
+
+	const handleCategoryName = (e: any) => {
+		setCategoryName(e.target.value);
+	};
+
+	// Runs on click of create button
+	const handleCreateCategory = async () => {
+		// Convert state for recordType to id stored in db for comparison below
+		let newRecordType = 0;
+
+		if (recordType === "Income") {
+			newRecordType = 1;
+		} else if (recordType === "Expenses") {
+			newRecordType = 2;
+		}
+
+		let data: newCategoryData = {
+			userId: currentUserId,
+			recordId: newRecordType,
+			categoryName: categoryName,
+		};
+
+		if (data.userId || data.recordId !== 0 || data.categoryName) {
+			try {
+				const response = await createCategoryAPI(data);
+
+				// Refreshes the data on page
+				refreshData();
+
+				// Close modal upon successful update
+				handleClose();
+
+				// Clear submitted info from state after creation
+				setRecordType("");
+				setCategoryName("");
+			} catch (err) {
+				if (typeof err === "string") {
+					setError(err);
+				} else if (err instanceof Error) {
+					setError(err.message);
+				}
+			}
+		}
+	};
+
+	return (
+		<Dialog open={openModal} fullWidth onClose={handleClose} sx={{ marginLeft: "auto", marginRight: "auto" }}>
+			<DialogTitle>Create Category</DialogTitle>
+			<DialogContent>
+				<Box component="form" sx={{ marginTop: "1rem" }}>
+					<Stack spacing={3}>
+						<FormControl sx={{ width: "100%", marginBottom: "1rem" }}>
+							<InputLabel htmlFor="income-or-expense">Type</InputLabel>
+							<Select value={recordType} onChange={handleRecordType} input={<OutlinedInput label="Type" id="income-or-expense" />}>
+								<MenuItem value={"Income"}>Income</MenuItem>
+								<MenuItem value={"Expenses"}>Expenses</MenuItem>
+							</Select>
+						</FormControl>
+						<TextField id="categoryName" label="Category Name" variant="outlined" sx={{ width: "100%" }} value={categoryName} onChange={handleCategoryName} />
+					</Stack>
+				</Box>
+			</DialogContent>
+			<DialogActions>
+				<Button variant="contained" size="large" onClick={handleCreateCategory}>
+					Create
+				</Button>
+			</DialogActions>
+		</Dialog>
+	);
+};
+
+export default CategoryCreationModal;
--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -5,19 +5,10 @@ import CategoryRow from "../components/CategoryRow";
 import styles from "./Settings.module.css";
 import { useCurrentUserContext } from "../context/currentUserContext";
 import whyMeme from "../assets/confused-white-persian-guardian.gif";
-import { Fab, Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, FormControl, InputLabel, MenuItem, OutlinedInput, Select, TextField, Stack } from "@mui/material";
+import { Fab } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
-import { createCategoryAPI } from "../apis/categories";
 import { useNavigate } from "react-router-dom";
-
-/* ====================================================
-// Type Declaration
-==================================================== */
-interface newCategoryData {
-	userId: number;
-	recordId: number;
-	categoryName: string;
-}
+import CategoryCreationModal from "../components/CategoryCreationModal";
 
 const Settings = () => {
 	const navigate = useNavigate();
@@ -26,7 +17,7 @@ const Settings = () => {
     // Context
     ==================================================== */
 
-	const { currentUserId, updateCurrentUser, categories, refreshData } = useCurrentUserContext();
+	const { updateCurrentUser, categories } = useCurrentUserContext();
 
 	/* ====================================================
     // Filter categories to income & expense types
@@ -35,6 +26,7 @@ const Settings = () => {
 	let incomeCategories = [];
 	let expenseCategories = [];
 
+	// Loop through categories array and assign them to incomeCategories & expenseCategories based on whether it belongs to income or expense
 	for (let i = 0; i < categories.length; i++) {
 		if (categories[i].recordId === 1) {
 			incomeCategories.push(categories[i]);
@@ -43,10 +35,12 @@ const Settings = () => {
 		}
 	}
 
+	// JSX for income & category rows
 	const incomeCategoriesToDisplay = incomeCategories.map((category) => <CategoryRow category={category} />);
 
 	const expenseCategoriesToDisplay = expenseCategories.map((category) => <CategoryRow category={category} />);
 
+	// JSX if no categories for income or expenses
 	const toDisplayIfNoCategories = (
 		<div className={styles.thisIsEmpty}>
 			<h3>This is empty</h3>
@@ -58,8 +52,6 @@ const Settings = () => {
     // Create new category modal
     ==================================================== */
 
-	const [error, setError] = useState<any>();
-
 	const [openModal, setOpenModal] = useState<boolean>(false);
 
 	// Opens dialog
@@ -70,60 +62,6 @@ const Settings = () => {
 	// Closes dialog
 	const handleClose = () => {
 		setOpenModal(false);
-	};
-
-	// Controls the value of select field
-	const [recordType, setRecordType] = useState<string>("");
-
-	// Controls the value for text field
-	const [categoryName, setCategoryName] = useState<string>("");
-
-	const handleRecordType = (e: any) => {
-		setRecordType(e.target.value);
-	};
-
-	const handleCategoryName = (e: any) => {
-		setCategoryName(e.target.value);
-	};
-
-	// Runs on click of create button
-	const handleCreateCategory = async () => {
-		// Convert state for recordType to id stored in db for comparison below
-		let newRecordType = 0;
-
-		if (recordType === "Income") {
-			newRecordType = 1;
-		} else if (recordType === "Expenses") {
-			newRecordType = 2;
-		}
-
-		let data: newCategoryData = {
-			userId: currentUserId,
-			recordId: newRecordType,
-			categoryName: categoryName,
-		};
-
-		if (data.userId || data.recordId !== 0 || data.categoryName) {
-			try {
-				const response = await createCategoryAPI(data);
-
-				// Refreshes the data on page
-				refreshData();
-
-				// Close modal upon successful update
-				handleClose();
-
-				// Clear submitted info from state after creation
-				setRecordType("");
-				setCategoryName("");
-			} catch (err) {
-				if (typeof err === "string") {
-					setError(err);
-				} else if (err instanceof Error) {
-					setError(err.message);
-				}
-			}
-		}
 	};
 
 	/* ====================================================
@@ -162,28 +100,7 @@ const Settings = () => {
 				</h2>
 			</div>
 			{/* Form Dialog, only visible when open */}
-			<Dialog open={openModal} fullWidth onClose={handleClose} sx={{ marginLeft: "auto", marginRight: "auto" }}>
-				<DialogTitle>Create Category</DialogTitle>
-				<DialogContent>
-					<Box component="form" sx={{ marginTop: "1rem" }}>
-						<Stack spacing={3}>
-							<FormControl sx={{ width: "100%", marginBottom: "1rem" }}>
-								<InputLabel htmlFor="income-or-expense">Type</InputLabel>
-								<Select value={recordType} onChange={handleRecordType} input={<OutlinedInput label="Type" id="income-or-expense" />}>
-									<MenuItem value={"Income"}>Income</MenuItem>
-									<MenuItem value={"Expenses"}>Expenses</MenuItem>
-								</Select>
-							</FormControl>
-							<TextField id="categoryName" label="Category Name" variant="outlined" sx={{ width: "100%" }} value={categoryName} onChange={handleCategoryName} />
-						</Stack>
-					</Box>
-				</DialogContent>
-				<DialogActions>
-					<Button variant="contained" size="large" onClick={handleCreateCategory}>
-						Create
-					</Button>
-				</DialogActions>
-			</Dialog>
+			<CategoryCreationModal openModal={openModal} handleClickOpen={handleClickOpen} handleClose={handleClose} />
 		</div>
 	);
 };
