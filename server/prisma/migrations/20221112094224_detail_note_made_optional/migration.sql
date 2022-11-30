be1bf904762fc0d401846f4a29c52a60af296636
ruyi <tan_ruyi@hotmail.com>
2022-11-30 10:28:34 +0800

    moved back-end to another repo

--- server/prisma/migrations/20221112094224_detail_note_made_optional/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "Expenses" ALTER COLUMN "detail" DROP NOT NULL,
-ALTER COLUMN "note" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "Income" ALTER COLUMN "detail" DROP NOT NULL,
-ALTER COLUMN "note" DROP NOT NULL;
