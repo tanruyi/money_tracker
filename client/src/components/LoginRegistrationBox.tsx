eeec5ddadbd10e8437c25542c022fb4c5b5ac800
ruyi <tan_ruyi@hotmail.com>
2023-01-04 16:37:38 +0800

    refactor: presentational component for welcome page created
    presentational component is for login/register box in welcome page

--- client/src/components/LoginRegistrationBox.tsx
+++ client/src/components/LoginRegistrationBox.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+/** @format */
+
+import React from "react";
+import styles from "./LoginRegistrationBox.module.css";
 
 const LoginRegistrationBox = () => {
-  return (
-    <div>LoginRegistrationBox</div>
-  )
-}
+	return (
+		<div className={styles.loginRegistrationBoxContainer}>
+			<p className={styles.loginRegistrationBoxHeadline}>Welcome back</p>
+            <button className={styles.loginRegistrationBoxButton}>Login</button>
+            <button className={styles.loginRegistrationBoxButton}>Create account</button>
+		</div>
+	);
+};
 
-export default LoginRegistrationBox
+export default LoginRegistrationBox;
