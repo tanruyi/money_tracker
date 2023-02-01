d974b64345099e1d24df236d0ad98e8b390ec9d7
ruyi <tan_ruyi@hotmail.com>
2023-02-01 13:13:58 +0800

    Dashboard: done

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,13 +1,59 @@
-import styles from './Dashboard.module.css'
+/** @format */
+
+import { ToggleButton, ToggleButtonGroup } from "@mui/material";
+import React, { useState } from "react";
+import DashboardChart from "../components/DashboardChart";
+import styles from "./Dashboard.module.css";
 
 const Dashboard = () => {
-  return (
-      <div className={styles.dashboardContainer}>
-          <div className={styles.dashboardChartContainer}>
-              <h2 className={styles.dashboardChartHeader}>Income</h2>
-          </div>
-    </div>
-  )
-}
+	const [chartPeriodType, setChartPeriodType] = useState<"Monthly" | "YTD">("Monthly");
 
-export default Dashboard
+	function handleIncomeChartType(e: React.MouseEvent<HTMLElement>, newChartType: "Monthly" | "YTD") {
+		if (newChartType !== null) {
+			setChartPeriodType(newChartType);
+		}
+	}
+
+	return (
+		<div className={styles.dashboardContainer}>
+			<div className={styles.dashboardChartContainer}>
+				<h2 className={styles.dashboardChartHeader}>Income</h2>
+				<ToggleButtonGroup
+					exclusive
+					value={chartPeriodType}
+					onChange={handleIncomeChartType}
+					sx={{
+						gridColumn: "3",
+						height: "4vh",
+						marginTop: "1vh",
+						width: "1vw",
+					}}
+				>
+					<ToggleButton value="Monthly">Monthly</ToggleButton>
+					<ToggleButton value="YTD">YTD</ToggleButton>
+				</ToggleButtonGroup>
+				<DashboardChart recordType={"Income"} periodType={chartPeriodType} />
+			</div>
+			<div className={styles.dashboardChartContainer}>
+				<h2 className={styles.dashboardChartHeader}>Expenses</h2>
+				<ToggleButtonGroup
+					exclusive
+					value={chartPeriodType}
+					onChange={handleIncomeChartType}
+					sx={{
+						gridColumn: "3",
+						height: "4vh",
+						display: "flex",
+						marginTop: "1vh",
+					}}
+				>
+					<ToggleButton value="Monthly">Monthly</ToggleButton>
+					<ToggleButton value="YTD">YTD</ToggleButton>
+				</ToggleButtonGroup>
+				<DashboardChart recordType={"Expenses"} periodType={chartPeriodType} />
+			</div>
+		</div>
+	);
+};
+
+export default Dashboard;
